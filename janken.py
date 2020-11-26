3fb0bcc9db895130ab033f0736c509b5894e5bb9
hikapyon <47267383+hikapyon@users.noreply.github.com>
2020-11-27 02:40:40 +0900

    0,1,2以外が入力された時の例外処理を追加

--- janken.py
+++ janken.py
@@ -2,31 +2,46 @@ import random
 h_list = ["グー","チョキ","パー"]
 class Your_Hand:
     def __init__(self):
+            
         n = int(input("グーは0,チョキは1,パーは2を入力してください"))
         self.n = n
         self.hand = h_list[self.n]
+        
 class Cp_Hand:
     def __init__(self):
         n = random.randint(0,2)
         self.n = n
         self.hand = h_list[self.n]
 
-your_hand = Your_Hand()
-cp_hand = Cp_Hand()
 def game(h1,h2):
-    print("ジャンケンポイ")
     if h1.n == h2.n:
+        print("ジャンケンポイ")
         print("あなた　コンピューター")
         print("{0} {1}".format(h1.hand,h2.hand))
         print("あいこです")
     elif((h1.n == 0)and(h2.n ==1))or((h1.n == 1)and(h2.n ==2))or((h1.n == 2)and(h2.n ==0)):
+        print("ジャンケンポイ")
         print("あなた　コンピューター")
         print("{0} {1}".format(h1.hand,h2.hand))
         print("あなたの勝ちです")
     else:
+        print("ジャンケンポイ")
         print("あなた　コンピューター")
-        print("{0} {1}".format(h1.hand,h2.hand))
+        print("{0} {1}".format(h1.hand,h2.hand))お
         print("あなたの負けです")
-
-game(your_hand,cp_hand)
+while True:
+    m = "qで終了、それ以外のキーでplay"
+    response = input(m)
+    if response == 'q':
+        break
+    try:
+        your_hand = Your_Hand()
+        cp_hand = Cp_Hand()
+        game(your_hand,cp_hand)
+    except IndexError:
+        print('0,1,2以外が入力されました、0,1,2のみを入力してください')
+        pass
+    except ValueError:
+        print('0,1,2以外が入力されました、0,1,2のみを入力してください')
+        pass
 

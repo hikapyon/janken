c600242fa5304db71e8e0ffd7a774fc5ae48ba3d
hikapyon <47267383+hikapyon@users.noreply.github.com>
2020-11-27 01:54:22 +0900

    後のためにプレーヤーの手とコンピューターの手をクラス化、勝ち負けを関数化した

--- janken.py
+++ janken.py
@@ -1,27 +1,32 @@
 import random
 h_list = ["グー","チョキ","パー"]
-your_n = int(input("グーは0,チョキは1,パーは2を入力してください"))
-your_hand = h_list[your_n]
-cp_n = random.randint(0,2)
-cp_hand = h_list[cp_n]
-print(your_hand)
-print(cp_hand)
+class Your_Hand:
+    def __init__(self):
+        n = int(input("グーは0,チョキは1,パーは2を入力してください"))
+        self.n = n
+        self.hand = h_list[self.n]
+class Cp_Hand:
+    def __init__(self):
+        n = random.randint(0,2)
+        self.n = n
+        self.hand = h_list[self.n]
 
+your_hand = Your_Hand()
+cp_hand = Cp_Hand()
+def game(h1,h2):
+    print("ジャンケンポイ")
+    if h1.n == h2.n:
+        print("あなた　コンピューター")
+        print("{0} {1}".format(h1.hand,h2.hand))
+        print("あいこです")
+    elif((h1.n == 0)and(h2.n ==1))or((h1.n == 1)and(h2.n ==2))or((h1.n == 2)and(h2.n ==0)):
+        print("あなた　コンピューター")
+        print("{0} {1}".format(h1.hand,h2.hand))
+        print("あなたの勝ちです")
+    else:
+        print("あなた　コンピューター")
+        print("{0} {1}".format(h1.hand,h2.hand))
+        print("あなたの負けです")
 
-        
+game(your_hand,cp_hand)
 
-if your_n == cp_n:
-    print("あなた コンピューター")
-    print("{} {}".format(your_hand,cp_hand))
-    print("あいこです")
-elif((your_n == 0)and(cp_n ==1))or((your_n == 1)and(cp_n ==2))or((your_n == 2)and(cp_n ==0)):
-    print("あなた コンピューター")
-    print("{} {}".format(your_hand,cp_hand))
-    print("あなたの勝ちです")
-else:
-    print("あなた コンピューター")
-    print("{} {}".format(your_hand,cp_hand))
-    print("あなたの負けです")
-
-
-    
